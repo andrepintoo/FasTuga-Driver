ac79dee2c33e22cde802211c75ca03862383644d
Yunikyon <anaritamsmartins@gmail.com>
2022-11-09 17:27:52 +0000

    Add RegisterActivity Layout Design

--- fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/activities/RegisterActivity.java
+++ fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/activities/RegisterActivity.java
@@ -1,8 +1,10 @@
-package ipleiria.taes.fastugadriver.activties;
+package ipleiria.taes.fastugadriver.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ipleiria.taes.fastugadriver.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
