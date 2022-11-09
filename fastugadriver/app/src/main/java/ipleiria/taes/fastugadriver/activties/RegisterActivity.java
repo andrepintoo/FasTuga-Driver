616be2e965a0a56edccd5a014a5aacc0fe9172e6
sfilipa <2201757@my.ipleiria.pt>
2022-11-09 17:40:08 +0000

    login layout

--- fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/activties/RegisterActivity.java
+++ fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/activties/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ipleiria.taes.fastugadriver.R;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
