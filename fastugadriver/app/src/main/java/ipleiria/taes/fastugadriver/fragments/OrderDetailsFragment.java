9b55d03dc1531961ac308eaaabb0e9a38ed13862
sfilipa <2201757@my.ipleiria.pt>
2022-11-26 01:22:47 +0000

    OrderDetailsFragment receiving data from AvailableOrdersFragment

--- fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/fragments/OrderDetailsFragment.java
+++ fastugadriver/app/src/main/java/ipleiria/taes/fastugadriver/fragments/OrderDetailsFragment.java
@@ -6,17 +6,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.JsonObject;
 
 import ipleiria.taes.fastugadriver.R;
@@ -27,11 +23,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrderDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OrderDetailsFragment extends Fragment {
 
     private static final String TAG = "OrderDetailsActivity";
@@ -42,47 +33,10 @@ public class OrderDetailsFragment extends Fragment {
     private TextView textLocationInput;
     private TextView textDistanceInput;
     private TextView textEarningInput;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OrderDetailsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AvailableOrdersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OrderDetailsFragment newInstance(String param1, String param2) {
-        OrderDetailsFragment fragment = new OrderDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        Fragment fragment = new MapFragment();
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
-    }
+    private Button buttonBack;
+    private Button buttonAccept;
+    private Button buttonClaim;
+    private Button buttonComplete;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -90,6 +44,8 @@ public class OrderDetailsFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_order_details, container, false);
+        Fragment map_fragment = new MapFragment();
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, map_fragment).commit();
 
         textOrderNumberInput = (TextView) view.findViewById(R.id.textOrderNumberInput);
         textClientInput = (TextView) view.findViewById(R.id.textClientInput);
@@ -97,6 +53,11 @@ public class OrderDetailsFragment extends Fragment {
         textLocationInput = (TextView) view.findViewById(R.id.textLocationInput);
         textDistanceInput = (TextView) view.findViewById(R.id.textDistanceInput);
         textEarningInput = (TextView) view.findViewById(R.id.textEarningInput);
+        buttonBack = (Button) view.findViewById(R.id.buttonBack);
+        buttonAccept = (Button) view.findViewById(R.id.buttonAccept);
+        buttonClaim = (Button) view.findViewById(R.id.buttonClaim);
+        buttonComplete = (Button) view.findViewById(R.id.buttonComplete);
+
         //textOrderNumberInput.setText("OrderID");
         textClientInput.setText("Information");
         textClientPhoneInput.setText("Phone Number");
@@ -104,31 +65,53 @@ public class OrderDetailsFragment extends Fragment {
         textDistanceInput.setText("Distance km");
         textEarningInput.setText("Earning €");
 
-        // Receive Order
-        Intent intent = getActivity().getIntent();
+        //receives the data
+        Bundle bundle = getArguments();
+        int orderID = 0;
+        Character orderStatus = null;
+        //stores in orderID
+        if (bundle != null) {
+            orderID = bundle.getInt("orderID");
+            orderStatus = bundle.getChar("orderStatus");
+        }
 
-        int orderID = 190;//Integer.parseInt(intent.getStringExtra("orderID"));
-        System.out.println("AQUI SUA PARVA -- DEBUG -- AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        System.out.println(intent.getStringExtra("orderID"));
-        System.out.println(orderID);
-        // Retrofit
+        if(orderStatus == 'R'){
+            buttonAccept.setVisibility(View.VISIBLE);
+            buttonComplete.setVisibility(View.GONE);
+            buttonClaim.setVisibility(View.GONE);
+        }
+
         OrderService service = RetrofitClient.getRetrofitInstance().create(OrderService.class);
         Call<OrderModelObject> order = service.getOrder(5);
 
+
+        int finalOrderID = orderID;
         order.enqueue(new Callback<OrderModelObject>() {
             @Override
             public void onResponse(Call<OrderModelObject> call, Response<OrderModelObject> response) {
-                Log.e(TAG, "onResponse: code : " + response.code());
-                JsonObject order = response.body().getOrder();
-                JsonObject delivered_by = order.getAsJsonObject("delivered_by");
-                Log.e(TAG, "onResponse: order : " +  delivered_by.get("name"));
-
-                textOrderNumberInput.setText(orderID);
+                textOrderNumberInput.setText(""+finalOrderID);
             }
 
             @Override
             public void onFailure(Call<OrderModelObject> call, Throwable t) {
-                Log.e(TAG, "onFaulure : "+ t.getMessage());
+                Log.e(TAG, "onFailure : "+ t.getMessage());
+            }
+        });
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Bundle sends data to the other fragment
+                Fragment fragment = new AvailableOrdersFragment();
+                replaceFragment(fragment);
+            }
+
+            public void replaceFragment(Fragment someFragment) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                //transaction.remove(AvailableOrdersFragment.this);
+                transaction.replace(R.id.navHostFragment, someFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
 
